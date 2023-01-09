d802774af4429878d5ff31ae9a232219fae221c1
Renner Nunes <rennercorrea@gmail.com>
2023-01-09 20:18:56 -0300

    Add CSS and Xpath tests

--- features/login.feature
+++ features/login.feature
@@ -10,11 +10,11 @@ Funcionalidade: Login
         Quando eu faço login como "t@t.com" e "123456"
         Então devo ser autenticado
         E devo ver "Tony Stark" na área logada
-
+@login_hapless
     Cenario: Senha inválida
         Quando eu faço login como "t@t.com" e "abc123456"
         Então não devo ser autenticado
-        E devo ver a mensagem "usuário e/ou senha inválidos"
+        E devo ver a mensagem "Usuário e/ou senha inválidos"
 
     Cenario: Usuário inválido
         Quando eu faço login como "abc@aabc.com" e "abc123456"
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -15,9 +15,23 @@ end
 Então("devo ver {string} na área logada") do |expect_name|
     expect(page).to have_text expect_name
 
-    user = find(:xpath, ".//span[contains(.,'#{expect_name}')]")
-    user2 = find('.sidebar-wrapper .user .info span') #css
+    user_xpath = find(:xpath, ".//span[contains(.,'#{expect_name}')]")
+    user_css = find('.sidebar-wrapper .user .info span')
 
-    expect(user.text).to  eql expect_name
-    expect(user2.text).to  eql expect_name
+    expect(user_xpath.text).to  eql expect_name
+    expect(user_css.text).to  eql expect_name
+end
+
+
+Então("não devo ser autenticado") do
+    js_script = 'return window.localStorage.getItem("default_auth_token");'
+    token = page.execute_script(js_script)
+    expect(token).to be nil
+end
+  
+Então("devo ver a mensagem {string}") do |expect_mensagem|
+    alert_xpath = find(:xpath, ".//div[contains(@class,'alert')]")
+    alert_css = find('.alert')
+    expect(alert_xpath.text).to eql expect_mensagem  
+    expect(alert_css.text).to eql expect_mensagem  
 end
